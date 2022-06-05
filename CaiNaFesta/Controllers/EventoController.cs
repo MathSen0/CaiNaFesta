4b1578ab5e7b45b0fa076e1c753733cd05c2fe46
Matheus Henrique Pessoal <maatheushenrique155@gmail.com>
2022-06-05 10:05:20 -0300

    <Matheus_Criação de controller e banco Contexto>

---
+++ CaiNaFesta/Controllers/EventoController.cs
@@ -0,0 +1,22 @@
+﻿using CaiNaFesta.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace CaiNaFesta.Controllers
+{
+    public class EventoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

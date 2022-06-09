ff3794109b6a1873b7864f0c37396aab6f26e810
MarcosBqProgramador <marcospaulodesouza0215@gmail.com>
2022-06-08 21:47:58 -0300

    08/06/2022, final da aula, foi o que deu...

---
+++ CaiNaFesta/Controllers/EventoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaiNaFesta.Controllers
+{
+    public class EventoController : Controller
+    {
+        public EventoController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

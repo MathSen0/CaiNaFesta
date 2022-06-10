0359405e2c1e8a9976596b64a1593d1f4978b62f
Matheus Henrique <88563681+MathSen0@users.noreply.github.com>
2022-06-10 19:43:15 -0300

    Update EventoModel.cs
    Correção dos campos de data e hora

--- CaiNaFesta/Models/EventoModel.cs
+++ CaiNaFesta/Models/EventoModel.cs
@@ -4,15 +4,14 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Dia { get; set; }
-        public string HoraInicio { get; set; }
-        public string HoraFim { get; set; }
+        public DateTime Data { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFim { get; set; }
         public string Estado { get; set; }
         public string Cidade { get; set; }  
         public string Descrição { get; set; }
         public string Email { get; set; }  
         public int Lotacao { get; set; }
 
-        //Coloquei os campos Dia, HoraInicio e HoraFim como String por que não apareceu as opções certas de vetores --Matheus
     }
 }
